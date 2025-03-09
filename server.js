d34b7aaa23b352e2ef93097b685d247d49b83a6f
unknown <omar.er3434@gmail.com>
2025-03-09 23:02:19 +0200

    Fix JSON Server to work with Vercel

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require("json-server");
+const { Low, Memory } = require("lowdb");
+const express = require("express");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(new Low(new Memory())); // استخدام الذاكرة بدلاً من الملفات
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+module.exports = server;
